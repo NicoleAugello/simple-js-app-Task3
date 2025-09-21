985e6d0c388ec4cd2eaab447c4cacf17c5ab3f52
NicoleAugello <nicole.augello@ontariotechu.net>
2025-09-21 17:52:09 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    { name: 'Jigglypuff', height: 5, types:  ['poision', 'bug']}
-    { name: 'Charmeleon', height: 11, types:  ['ground', 'steel']}
+    { name: 'Jigglypuff', height: 5, types:  ['poision', 'bug']},
+    { name: 'Charmeleon', height: 11, types:  ['ground', 'steel']},
     { name: 'Raichu', height: 8, types:  ['fire', 'flying']}
 ];
