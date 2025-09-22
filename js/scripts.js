f8d3a1de42e7872a7b434eac27b756ac2d53e329
NicoleAugello <nicole.augello@ontariotechu.net>
2025-09-22 10:52:09 -0400

    Task3-Part1
    for loop + document.write() name and height

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ let pokemonList = [
     { name: 'Charmeleon', height: 11, types:  ['ground', 'steel']},
     { name: 'Raichu', height: 8, types:  ['fire', 'flying']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`<h1>${pokemonList[i].name} (Height: ${pokemonList[i].height})</h1>`);
+}
